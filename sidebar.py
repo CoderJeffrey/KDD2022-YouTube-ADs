9ab65c74fa027c31cf476fa2ec6c0c7f575b17c6
Jeffrey Liu <jliu5021@usc.edu>
2022-07-13 11:56:40 -0700

    Updated Version for ads extraction

---
+++ sidebar.py
@@ -0,0 +1,78 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
+from selenium.common.exceptions import StaleElementReferenceException
+import time
+from selenium.webdriver.support.ui import WebDriverWait
+
+import json
+
+#extract videos from a file
+videos = set()
+
+#change to the file with video name
+with open("EXAMPLE_sidebarid.txt", "r") as f:
+    lines = f.readlines()
+for p in lines:
+    videos.add(p.strip())
+f.close()
+
+prev = ""
+info = []
+
+for l in videos:
+    #connect to the youtube video page
+    toget = "https://www.youtube.com/watch?v=" + l
+    print("toget",toget)
+
+
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    chrome_options.add_argument("--public")
+    driver = webdriver.Chrome(ChromeDriverManager().install(),chrome_options=chrome_options)
+
+    for i in range(1,2):
+        try:
+            driver.get(toget)
+            #get the innerHTML of the video page
+            html = driver.execute_script("return document.getElementsByTagName('html')[0 ].innerHTML")
+
+            all_info = str(html)
+            #return the extracted metadata to S_RESULT.txt (change s_RESULT.txt to any output file you prefer)
+            fname = "s_RESULT.txt"
+
+            if "promotedSparklesWebRenderer" in all_info:
+                place = all_info.find("promotedSparklesWebRenderer")
+                info = all_info[place:place+2000]
+
+                #extract the title tag
+                if "title" in info:
+                    title_place = info.find("title")
+                    end_place = info[title_place:].find("}")
+                    line_sub_title = info[title_place+22:title_place+end_place-1]
+
+                #extract the description tag
+                if "description" in info:
+                    title_place = info.find("description")
+                    end_place = info[title_place:].find("}")
+                    line_sub_description = info[title_place+28:title_place+end_place-1]
+
+                #extract the website URL
+                if "websiteText" in info:
+                    title_place = info.find("websiteText")
+                    end_place = info[title_place:].find("}")
+                    line_sub_website = info[title_place+28:title_place+end_place-1]
+
+            """
+            output the content is the following format
+            video id | title | description | website url  
+            """
+            with open(fname, "w", encoding="utf-8") as f:
+                f.write(f"{l} | ")
+                f.write(f"{line_sub_title} | ")
+                f.write(f"{line_sub_description} | ")
+                f.write(f"{line_sub_website} \n")
+            time.sleep(5)            
+        except:
+            continue
+    driver.close() 
---
+++ video.py
@@ -0,0 +1,64 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
+from selenium.common.exceptions import StaleElementReferenceException
+import time
+from selenium.webdriver.support.ui import WebDriverWait
+
+videos = set()
+#visited video ads (to filter out duplicated video ads)
+visited = set()
+
+with open("EXAMPLE_videoid.txt","r") as f:
+    lines = f.readlines()
+for line in lines:
+    videos.add(line.strip())
+f.close()
+prev = ""
+
+for l in videos:
+    #connect to the Youtube video page
+    toget = "https://www.youtube.com/watch?v=" + l
+    print("toget",toget)
+
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--public")
+    driver = webdriver.Chrome(ChromeDriverManager().install(),chrome_options=chrome_options)
+
+    #store the result in v_RESULT.txt (change to any other name you prefer)
+    output = open("v_RESULT.txt","w", encoding="utf-8")
+   
+
+    for i in range(1,3):
+        try:
+            driver.get(toget)
+            driver.refresh()
+            
+            #get the innerHTML of the video page
+            html = driver.execute_script("return document.getElementsByTagName('html')[0].innerHTML")
+            commas = str(html).split(",")
+
+            #get the adVideo id tag
+            for line in commas:            
+                if('adVideoId"' in str(line)):
+                    reverse = line[::-1]
+                    # print(reverse + " REVERSED \n ")
+                    end = reverse.find('\"')
+                    start = reverse[end+1:].find('\"') + (end+1)
+                    ad_video_id = str(line[-start: -end-1])
+
+                    #if ad_video_id is unique, add them to the final output file
+                    if ad_video_id not in visited:
+                        output.write(ad_video_id +"\n")
+                        visited.add(ad_video_id)
+
+            #refresh the video page to extract updated meta data
+            time.sleep(5)
+            driver.refresh()
+            driver.switch_to_window(driver.window_handles[1])
+            driver.refresh()
+        
+        except:
+            continue
+    output.close()
+    driver.close()
